226d3481e4c7d611451899eb78bc67db5eba8b49
zeynepecekiris <zeynep.ece933@gmail.com>
2025-03-31 16:35:01 +0300

    remove usestate from parent repo

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
 import './App.css'
-import Product from './product'
+import Product from './Product'
+import Container from './Container'
 
 function App() {
-  const productName = "Buzdolabı";
-
+  
   
   return (
     <div>
-      <Product productName = "Ayakkabı" price={3200}/>
-      <Product productName = "pantolon" price= {950} />
-      <Product productName = "ruj" price= {50} />
-      <Product productName = "silikon" price= {90} />
-      <Product productName={productName} price = {15000} />
+      <Container>
+      <Product productName="Ayakkabı" price="300"/>
+      </Container>
+     
 
     </div>
   )
